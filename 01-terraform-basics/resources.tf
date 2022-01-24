3021f6170c18b2f8459eee41ba0ad1931ae551b0
Mahbub8 <mahbubaliazad8@hotmail.com>
2022-01-24 19:12:25 +0530

    first commit

---
+++ 01-terraform-basics/resources.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "my_s3_bucket" {
+  bucket = "my-s3-bucket-azad-rangak-002"
+  versioning {
+    enabled = true
+  }
+}
+
+resource "aws_iam_user" "my_iam_user" {
+  name = "my_iam_user_abc_updated"
+}
+
+# STATE:
+# DESIRED # KNOWN # ACTUAL
